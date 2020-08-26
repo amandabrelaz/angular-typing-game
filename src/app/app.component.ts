7c29e0e8524d961fa7475894b251fee92e85183f
brelaz <44024412+brelaz@users.noreply.github.com>
2020-08-26 19:35:42 +0100

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-typing-game';
+
+  //create random texts from lorem package
+  randomText = lorem.sentence();
+
+  enteredText = '';
+
+  //if the game is solved or not
+  solved=false
+
+  //method to check the user input
+  onInput(value:string){
+    this.enteredText = value;
+    if(value === this.randomText){
+      this.solved=true;
+    }
+  }
+
+  //method to compare between each letter of enteredText and each letter of ramdonText
+  compare(enteredLetter: string, randomLetter: string){
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect'
+  }
+
 }
